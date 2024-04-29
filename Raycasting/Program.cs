f19043a6f9ceb97349313d0f5dec38f1248c0e2c
Iziny <yoanndewilde@gmail.com>
2024-04-29 10:14:35 +0200

    Ajoutez des fichiers projet.

---
+++ Raycasting/Program.cs
@@ -0,0 +1,15 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "Raycasting");
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.White);
+
+    Raylib.DrawText("Texte", 200, 200, 30, Color.Black);
+    Raylib.DrawLine(100, 100, 200, 200, Color.Red);
+
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
